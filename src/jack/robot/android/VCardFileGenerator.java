19a8426398d8a4a7819a94420a9687b60ba7e047
bigbug <bigbug@vpghqdpbigdata100>
2014-11-28 11:45:58 +0800

    .

--- src/jack/robot/android/VCardFileGenerator.java
+++ src/jack/robot/android/VCardFileGenerator.java
@@ -15,29 +15,29 @@ public class VCardFileGenerator {
 	int max=Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws IOException {
-		//读取手机号码列表，将其转换成vcf文件
+//		//读取手机号码列表，将其转换成vcf文件
 ////		File in = new File("/home/bigbug/adt-workspace/data/phone-10k-congying.txt");
-//		File in = new File("/home/bigbug/adt-workspace/data/zhangyun/test-1"); //test400");  //寿险1.tsv");
-//		String path = "/home/bigbug/adt-workspace/vcards/test"; // all  test
-//		File out = new File(path);
-//		int MaxNumber = 800; //每个vcf文件中最多的联系人数目；  400
-//		if(args.length==2){
-//			in = new File(args[0]);
-//			out= new File(args[1]);
-//		}else{
-//			System.err.println("Using default input/output files: \n");
-//		}
-//		cells2vcf(in,out,MaxNumber);
+		File in = new File("/home/bigbug/adt-workspace/data/zhangyun/产险.tsv"); //test400");  //寿险1.tsv");
+		String path = "/home/bigbug/adt-workspace/vcards/产险"; // all  test
+		File out = new File(path);
+		int MaxNumber = 800; //每个vcf文件中最多的联系人数目；  400
+		if(args.length==2){
+			in = new File(args[0]);
+			out= new File(args[1]);
+		}else{
+			System.err.println("Using default input/output files: \n");
+		}
+		cells2vcf(in,out,MaxNumber);
 		
 		
-		
-		//读取一组vcf文件，按照指定的最大数目，重新生成一组vcf文件
-		//刚开始的时候，每个vcf中分配400个手机号。后来将没有跑完的vcf重新合并，改为每个vcf分配800个手机号；
-		File inDir = new File("/home/bigbug/adt-workspace/result/103/vcards/");
-		File outDir = new File("/home/bigbug/adt-workspace/result/103/vcards/800perVcf");
-		int MaxNumber = 800;
-		
-		rewrite(FileTools.filter(inDir, "vcf", 2),outDir,MaxNumber);
+//		//重新调整每个vcf文件中的联系人数目
+//		//输入：一组vcf文件，按照指定的最大数目，
+//		//输出：重新生成一组vcf文件
+//		//刚开始的时候，每个vcf中分配400个手机号。后来将没有跑完的vcf重新合并，改为每个vcf分配800个手机号；
+//		File inDir = new File("/home/bigbug/adt-workspace/result/103/vcards/");
+//		File outDir = new File("/home/bigbug/adt-workspace/result/103/vcards/800perVcf");
+//		int MaxNumber = 800;		
+//		rewrite(FileTools.filter(inDir, "vcf", 2),outDir,MaxNumber);
 		
 		System.out.println("=========Finished!========");
 		
@@ -78,14 +78,20 @@ public class VCardFileGenerator {
 //		Collections.shuffle(phones);
 		
 		gen.contacts = new HashSet<Contact>();
+		int err = 0;
 		for(String s :phones){
 			try{
 				long cell = Long.parseLong(s);
 				Contact c = new Contact(cell);
 				gen.contacts.add(c);
 			}catch(Exception e){
-				System.err.println("ignore cellphone format error:"+s);
+				err++;
+				System.err.println("illegal phone number: "+s);
+				continue;
 			}
+		}
+		if(err>0){
+			System.err.println("Found "+err+" illegal phone numbers.");
 		}
 		
 		// 分为多个文件；
